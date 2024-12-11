d503b54f4219806c1c49f208ad32f72bfd1703e5
Navneet Kaur <navkaurneet1@gmail.com>
2024-12-10 22:11:43 -0500

    Update __init__.py

--- function/__init__.py
+++ function/__init__.py
@@ -1,6 +1,25 @@
-import logging
 import azure.functions as func
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
+import logging
+ 
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+ 
+@app.route(route="http_trigger1")
+def http_trigger1(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    return func.HttpResponse("Hello, World!", status_code=200)
+ 
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+ 
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. This is the assignment3 Azure Function Pipeline.",
+             status_code=200
+        )
